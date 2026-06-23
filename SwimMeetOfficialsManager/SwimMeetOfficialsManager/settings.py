e1de777d06d0e542aba2f33790b3bd1be391019d
Gitpixel-debug <zorro.parth@gmail.com>
2026-06-23 09:33:26 -0700

    Update settings.py to use dj_database_url for DB

--- SwimMeetOfficialsManager/SwimMeetOfficialsManager/settings.py
+++ SwimMeetOfficialsManager/SwimMeetOfficialsManager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,10 +76,11 @@ WSGI_APPLICATION = 'SwimMeetOfficialsManager.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Vercel functions should not depend on database-backed sessions for auth state.
